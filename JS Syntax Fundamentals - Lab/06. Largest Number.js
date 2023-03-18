9e0fe650fae1bf75597a08ff85472e2dfacb8c19
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-03-18 22:00:17 +0200

    Initial commit

--- JS Syntax Fundamentals - Lab/06. Largest Number.js
+++ JS Syntax Fundamentals - Lab/06. Largest Number.js
@@ -1,1 +1,15 @@
 
+function largestNum(num1, num2, num3) {
+  let largest;
+  if (num1 > num2 && num1 > num3) {
+    largest = num1;
+  } else if (num2 > num1 && num2 > num3) {
+    largest = num2;
+  } else if (num3 > num1 && num3 > num2) {
+    largest = num3;
+  }
+  console.log(`The largest number is ${largest}.`);
+}
+
+largestNum(5, -3, 16)
+largestNum(-3, -5, -22.5)
