0fa2ffe86fac15500b533052b9809a1feb53e1d0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-03-18 21:59:52 +0200

    Initial commit

--- JS Syntax Fundamentals - Lab/05.Math Operations.js
+++ JS Syntax Fundamentals - Lab/05.Math Operations.js
@@ -1,1 +1,48 @@
 
+// function simpleCalc(number1, number2, operator){
+//     if (operator === '+'){
+//         console.log(number1 + number2)
+//     }
+//     else if (operator === '-'){
+//         console.log(number1 - number2)
+//     }
+//     else if (operator === '*'){
+//         console.log(number1 * number2)
+//     }
+//     else if (operator === '/'){
+//         console.log(number1 / number2)
+//     }
+//     else if (operator === '%'){
+//         console.log(number1 % number2)
+//     }
+//     else if (operator === '**'){
+//         console.log(number1 ** number2)
+//     }
+// }
+
+function simpleCalc(firstNum, secondNum, operator) {
+  let result;
+  switch (operator) {
+    case "+":
+      result = firstNum + secondNum;
+      break;
+    case "-":
+      result = firstNum - secondNum;
+      break;
+    case "/":
+      result = firstNum / secondNum;
+      break;
+    case "*":
+      result = firstNum * secondNum;
+      break;
+    case "%":
+      result = firstNum % secondNum;
+      break;
+    case "**":
+      result = firstNum ** secondNum;
+      break;
+  }
+  console.log(result);
+}
+
+simpleCalc(5, 6, "+");
