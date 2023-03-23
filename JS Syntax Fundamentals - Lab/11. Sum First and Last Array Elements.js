a62e4f1d243755ca06e60a07a940207c062bb352
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-03-23 10:02:48 +0200

    Initial commit

--- JS Syntax Fundamentals - Lab/11. Sum First and Last Array Elements.js
+++ JS Syntax Fundamentals - Lab/11. Sum First and Last Array Elements.js
@@ -1,1 +1,13 @@
 
+
+function sumFirstLast(inputArr){
+    first = inputArr[0]
+    // last = inputArr[inputArr.length-1]
+    last = inputArr.pop()
+    final = first + last
+    console.log(final)
+}
+
+sumFirstLast([20, 30, 40])
+sumFirstLast([10, 17, 22, 33])
+sumFirstLast([11, 58, 69])
