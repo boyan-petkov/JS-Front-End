e9eceabe980c93b6a389def8a30a4fa4656e33d9
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-03-23 10:03:40 +0200

    Initial commit

--- JS Syntax Fundamentals - Lab/08. Circle Area.js
+++ JS Syntax Fundamentals - Lab/08. Circle Area.js
@@ -1,1 +1,15 @@
+function circleArea(number){
+    let argumentType = typeof number
+    let area;
+    if (argumentType === 'number'){
+        area = (Math.PI * number * number).toFixed(2)
+        console.log(area)
+    }
+    else{
+        console.log(`We can not calculate the circle area, because we receive a ${argumentType}.`)
+    }
+}
 
+circleArea(5)
+circleArea('15')
+circleArea(true)
