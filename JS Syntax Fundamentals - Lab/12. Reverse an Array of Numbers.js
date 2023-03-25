8eb7af1c96c32335f2d4beedbfbfd8e10597c2b7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-03-25 19:22:51 +0200

    Initial commit

--- JS Syntax Fundamentals - Lab/12. Reverse an Array of Numbers.js
+++ JS Syntax Fundamentals - Lab/12. Reverse an Array of Numbers.js
@@ -1,1 +1,14 @@
+function reverseArray(num, inputArr ){
+    let final= [];
+    let printArr = []
+    for (let i=0; i<num; i++){
+        final.push(inputArr[i])
+    }
+    for (let i=final.length -1; i>=0; i--)
+    printArr.push(final[i])
 
+    console.log(printArr.join(" "))
+}
+
+
+reverseArray(3, [10, 20, 30, 40, 50])
