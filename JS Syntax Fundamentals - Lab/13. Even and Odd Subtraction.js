47890d611286335d2247c1a6b0050c76f49b3356
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-03-25 19:23:12 +0200

    Initial commit

--- JS Syntax Fundamentals - Lab/13. Even and Odd Subtraction.js
+++ JS Syntax Fundamentals - Lab/13. Even and Odd Subtraction.js
@@ -1,1 +1,27 @@
 
+function evenOddSubtraction(array){
+    let evens =[];
+    let odds = [];
+    let evenSum =0;
+    let oddSum=0;
+    for (let i=0; i<array.length; i++){
+        if (array[i] % 2 === 0){
+            evens.push(array[i])
+        }
+        else{
+            odds.push(array[i])
+        }
+    }
+    for (let i=0; i<evens.length; i++){
+        evenSum += evens[i]
+    }
+    for (let i=0; i<odds.length; i++){
+        oddSum += odds[i]
+    }
+    
+    console.log(evenSum - oddSum)
+}
+
+evenOddSubtraction([1,2,3,4,5,6])
+evenOddSubtraction([3,5,7,9])
+evenOddSubtraction([2,4,6,8,10])
