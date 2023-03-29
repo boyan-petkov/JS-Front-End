0dc1d5797678617d6dd36fa7d78c4a82d843a97f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-03-29 13:32:08 +0300

    Initial commit

--- JS Syntax Fundamentals - Lab/16. Count String Occurrences.js
+++ JS Syntax Fundamentals - Lab/16. Count String Occurrences.js
@@ -1,1 +1,18 @@
 
+function solve(text, word) {
+  let count = 0;
+  let words = text.split(" ");
+  for (let i = 0; i < words.length; i++){
+    if (words[i]=== word){
+        count ++
+    }
+  }
+  console.log(count)
+}
+
+solve('This is a word and it also is a sentence',
+'is'
+)
+solve('softuni is great place for learning new programming languages',
+'softuni'
+)
