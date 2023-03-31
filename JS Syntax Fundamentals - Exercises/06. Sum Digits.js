5747c97a2bd256efc7dcb2d8ee05e684d02880bd
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-03-31 10:48:16 +0300

    Initial commit

--- JS Syntax Fundamentals - Exercises/06. Sum Digits.js
+++ JS Syntax Fundamentals - Exercises/06. Sum Digits.js
@@ -1,1 +1,15 @@
 
+function sumDigits(number) {
+  let numberAsString = number.toString();
+  let resultSum = 0;
+  for (let i = 0; i < numberAsString.length; i++) {
+    resultSum += parseInt(numberAsString[i]);
+  }
+  console.log(resultSum);
+}
+
+
+
+sumDigits(245678);
+sumDigits(97561);
+sumDigits(543);
