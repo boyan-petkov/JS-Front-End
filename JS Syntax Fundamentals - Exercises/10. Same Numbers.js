8dbb09cd9ebbf75f5e1aecbf21e26426f9c5f5c4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-01 10:23:41 +0300

    Initial commit

--- JS Syntax Fundamentals - Exercises/10. Same Numbers.js
+++ JS Syntax Fundamentals - Exercises/10. Same Numbers.js
@@ -1,1 +1,19 @@
 
+function sameNumbers(number) {
+  let condition = true;
+  let sumNumber = 0;
+  numberToString = number.toString().split("");
+  let first = numberToString[0];
+  numberToString.forEach((element) => {
+    sumNumber += Number(element);
+    if (first !== element) {
+      condition = false;
+    }
+  });
+  console.log(condition);
+  console.log(sumNumber);
+}
+
+sameNumbers(2245);
+sameNumbers(2222222);
+sameNumbers(1234);
