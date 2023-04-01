9bbc8c50d496bec8739d0922a6488d2726320b98
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-01 10:23:26 +0300

    Initial commit

--- JS Syntax Fundamentals - Exercises/09. Fruit.js
+++ JS Syntax Fundamentals - Exercises/09. Fruit.js
@@ -1,1 +1,10 @@
 
+
+
+function fruits(fruitType, weight, pricePerKg){
+    weight /= 1000;
+    let moneyNeeded = (weight * pricePerKg).toFixed(2);
+    console.log(`I need $${moneyNeeded} to buy ${weight.toFixed(2)} kilograms ${fruitType}.`);
+}
+fruits('orange', 2500, 1.80)
+fruits('apple', 1563, 2.35)
