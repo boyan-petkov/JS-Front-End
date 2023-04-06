c5e0cc7ac23a52b8f697384455571c171eb8ae8d
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-06 08:57:25 +0300

    Initial commit

--- JS Syntax Fundamentals - Exercises/12. Cooking by Numbers.js
+++ JS Syntax Fundamentals - Exercises/12. Cooking by Numbers.js
@@ -1,1 +1,25 @@
+function cookingByNumbers(numberAsStr, ...operations) {
+  number = Number(numberAsStr);
 
+  operations.forEach((operator) => {
+    switch (operator) {
+      case "chop":
+        number /= 2;
+        break;
+      case "dice":
+        number = Math.sqrt(number);
+        break;
+      case "spice":
+        number++;
+        break;
+      case "bake":
+        number *= 3;
+        break;
+      case "fillet":
+        number -= number * 0.2;
+        break;
+    }
+    console.log(number);
+  });
+}
+cookingByNumbers("9", "dice", "spice", "chop", "bake", "fillet");
