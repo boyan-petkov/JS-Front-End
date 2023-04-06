a4107e0f987d0e3db3c3165ff3e264766619a9d4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-06 08:56:35 +0300

    Initial commit

--- JS Syntax Fundamentals - Exercises/15. List Of Names.js
+++ JS Syntax Fundamentals - Exercises/15. List Of Names.js
@@ -1,1 +1,16 @@
+function listOfNames(namesArray) {
+  let sorted = [...namesArray].sort();
+  for (let i = 0; i < sorted.length; i++) {
+    console.log(`${i + 1}.${sorted[i]}`);
+  }
+}
 
+
+
+function solve(input) {
+    input.sort((a,b) => a.localeCompare(b));
+    for(let i = 0; i < input.length;i++){
+        console.log(`${i+1}.${input[i]}`);
+    }
+}
+listOfNames(["John", "Bob", "Christina", "Ema"]);
