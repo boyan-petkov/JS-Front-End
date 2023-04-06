4162cbfeab6a520e436c80e8fef3cb2b274afbed
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-06 08:56:51 +0300

    Initial commit

--- JS Syntax Fundamentals - Exercises/14. Print every N-th Element from an Array.js
+++ JS Syntax Fundamentals - Exercises/14. Print every N-th Element from an Array.js
@@ -1,1 +1,11 @@
+function everyNthElement(numbers, step) {
+  let finalArray = [];
+  for (let i = 0; i < numbers.length; i += step) {
+    finalArray.push(numbers[i]);
+  }
+  console.log(`${finalArray.join('\n')}`);
+}
 
+everyNthElement(["5", "20", "31", "4", "20"], 2);
+everyNthElement(["dsa", "asd", "test", "tset"], 2);
+everyNthElement(["1", "2", "3", "4", "5"], 6);
