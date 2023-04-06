a01e6119e042583baba42c51bc1299e067a7bc0f
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-06 08:57:09 +0300

    Initial commit

--- JS Syntax Fundamentals - Exercises/13. Array Rotation.js
+++ JS Syntax Fundamentals - Exercises/13. Array Rotation.js
@@ -1,1 +1,13 @@
+function arrayRotation(numbers, rotations) {
+  let array = [];
+  for (let i = 0; i < rotations; i++) {
+    let currentFirst = numbers.shift();
+    numbers.push(currentFirst);
+  }
+  console.log(`${numbers.join(" ")}`)
+}
 
+
+arrayRotation([51, 47, 32, 61, 21], 2);
+arrayRotation([32, 21, 61, 1], 4);
+arrayRotation([2, 4, 15, 31], 5);
