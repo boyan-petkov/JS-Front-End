365ce8b1070ad92a9783a85e55443e209f0a5aff
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-07 22:54:03 +0300

    Initial commit

--- JS Syntax Fundamentals - Exercises/19-string-substring.js
+++ JS Syntax Fundamentals - Exercises/19-string-substring.js
@@ -1,1 +1,18 @@
 
+function stringSubstring(word, text) {
+    let textArr = text.split(' ');
+    flag = true;
+
+    for(el of textArr) {
+        if (el.toLowerCase() == word) {
+            console.log(word);
+            flag = false
+            break;
+        }
+    }
+            
+    if (flag) {
+        console.log(`${word} not found!`)
+    }
+
+}
