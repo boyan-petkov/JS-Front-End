ac548324e13b53edf26970880722c39b7bf4c6c4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-10 15:19:20 +0300

    Initial commit

--- Functions and Statements - Lab/04-orders.js
+++ Functions and Statements - Lab/04-orders.js
@@ -1,1 +1,23 @@
 
+function orders(product, quantity) {
+  let finalPrice = 0;
+  switch (product) {
+    case "coffee":
+      finalPrice = 1.5 * quantity;
+      break;
+    case "water":
+      finalPrice = 1.0 * quantity;
+      break;
+    case "coke":
+      finalPrice = 1.4 * quantity;
+      break;
+    case "snacks":
+      finalPrice = 2 * quantity;
+      break;
+  }
+  return (finalPrice.toFixed(2))
+}
+
+console.log(
+    orders('water', 5)
+)
