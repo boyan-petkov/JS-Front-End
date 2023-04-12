05d324473185bcbab299cf2490570e4ceda3a7fc
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-12 13:58:21 +0300

    Initial commit

--- Functions and Statements - Exercises/02-add-and-subtract.js
+++ Functions and Statements - Exercises/02-add-and-subtract.js
@@ -1,1 +1,14 @@
 
+function sum(first, second, third) {
+  let final;
+  let result = first + second;
+  subtract(result, third);
+  function subtract(result, third) {
+    final = result - third;
+  }
+  return final;
+}
+
+console.log(sum(23, 6, 10));
+console.log(sum(1, 17, 30));
+console.log(sum(42, 58, 100));
