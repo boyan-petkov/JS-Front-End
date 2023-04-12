42e53df4b73effb513d3de4f6963958e8fd2cefd
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-12 13:58:06 +0300

    Initial commit

--- Functions and Statements - Exercises/01-smallest-of-three-numbers.js
+++ Functions and Statements - Exercises/01-smallest-of-three-numbers.js
@@ -1,1 +1,16 @@
 
+function smallestOfThree(first, second, third) {
+  let smallest = first;
+  if (second < smallest) {
+    smallest = second;
+  }
+  if (third < smallest) {
+    smallest = third;
+  }
+  return smallest;
+}
+
+console.log(smallestOfThree(2, 5, 3));
+console.log(smallestOfThree(600, 342, 123));
+console.log(smallestOfThree(25, 21, 4));
+console.log(smallestOfThree(2, 2, 2));
