394903483d02f22337d761a63a5f7c2cebc76e7e
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-12 13:59:51 +0300

    Initial commit

--- Functions and Statements - Exercises/03-characters-in-range.js
+++ Functions and Statements - Exercises/03-characters-in-range.js
@@ -1,1 +1,24 @@
 
+function charsInRange(char1, char2) {
+  let chars = [];
+  let char1Num = char1.charCodeAt();
+  let char2Num = char2.charCodeAt();
+  let start;
+  let end;
+  if (char1Num < char2Num) {
+    start = char1Num;
+    end = char2Num;
+  } else {
+    start = char2Num;
+    end = char1Num;
+  }
+  for (let i = start + 1; i < end; i++) {
+    chars.push(String.fromCharCode(i));
+  }
+  return chars.join(" ");
+}
+
+
+console.log(charsInRange("a", "d"));
+console.log(charsInRange("#", ":"));
+console.log(charsInRange("C", "#"));
