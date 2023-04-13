dc3b82bee308c9ff893ade569cae5daa79b244df
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-13 08:57:34 +0300

    Initial commit

--- Functions and Statements - Exercises/07-nxn-matrix.js
+++ Functions and Statements - Exercises/07-nxn-matrix.js
@@ -1,1 +1,10 @@
 
+function matrix(n) {
+    for(let i = 0; i < n; i++) {
+        let output = '';
+        for (let j = 0; j < n; j++) {
+            output += String(n) + ' ';
+        }
+        console.log(output)
+    }
+}
