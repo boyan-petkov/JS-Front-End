994b2c4a52fb03edb29496d1ff256e7f9bf557d7
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-13 08:55:45 +0300

    Initial commit

--- Functions and Statements - Exercises/05-palindrome-integers.js
+++ Functions and Statements - Exercises/05-palindrome-integers.js
@@ -1,1 +1,15 @@
 
+function palindromeIntegers(numbers) {
+  for (let i = 0; i < numbers.length; i++) {
+    let current = numbers[i].toString();
+    let currentReversed = current.split("").reverse().join("");
+    if (current === currentReversed) {
+      console.log("true");
+    } else {
+      console.log("false");
+    }
+  }
+}
+
+palindromeIntegers([123, 323, 421, 121]);
+palindromeIntegers([32, 2, 232, 1010]);
