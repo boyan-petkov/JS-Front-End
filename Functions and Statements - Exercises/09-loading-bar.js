3362ea6944638bca347ccd7ca9390e5e6f165ff0
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-13 08:58:45 +0300

    Initial commit

--- Functions and Statements - Exercises/09-loading-bar.js
+++ Functions and Statements - Exercises/09-loading-bar.js
@@ -1,1 +1,14 @@
 
+function loadingBar(number) {
+    let bar = '[..........]'
+
+    if (number == 100) {
+        console.log('100% Complete!')
+    } else {
+        for (let i = 0; i < number; i+=10) {
+            bar = bar.replace('.', '%')
+        }
+        console.log(`${number}% ${bar}`);
+        console.log('Still loading...');  
+    }
+}
