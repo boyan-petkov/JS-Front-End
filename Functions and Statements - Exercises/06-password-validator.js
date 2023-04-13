12624217551c8ff7ab4b7eb778eae4eca25c465c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-13 08:56:04 +0300

    Initial commit

--- Functions and Statements - Exercises/06-password-validator.js
+++ Functions and Statements - Exercises/06-password-validator.js
@@ -1,1 +1,27 @@
 
+function passwordValidator(password) {
+  let condition1;
+  let condition2;
+  let condition3;
+  let passwordValid = true;
+
+  passwordLength(password);
+  lettersAndDigits(password);
+  function passwordLength(password) {
+    if (password.length >= 6 && password.length <= 10) {
+      condition1 = true;
+    } else {
+      condition1 = "Password must be between 6 and 10 characters";
+      passwordValid = false;
+    }
+  }
+
+  function lettersAndDigits(password) {
+    for (let i = 0; i < password.length; i++) {
+      let currentChar = password[i];
+      
+    }
+  }
+}
+
+passwordValidator("b1oya1");
