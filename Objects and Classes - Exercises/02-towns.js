b4582932f6579744d786a75d7fd7c90dcc615dd4
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-19 09:54:57 +0300

    Initial commit

--- Objects and Classes - Exercises/02-towns.js
+++ Objects and Classes - Exercises/02-towns.js
@@ -1,1 +1,18 @@
 
+function townsFunc(input) {
+  let towns = [];
+  for (const line of input) {
+    let [town, latitude, longitude] = line.split(" | ");
+    towns.push({
+      town,
+      latitude: Number(latitude).toFixed(2),
+      longitude: Number(longitude).toFixed(2),
+    });
+  }
+
+  towns.forEach((element) => {
+    console.log(element);
+  });
+}
+
+townsFunc(["Sofia | 42.696552 | 23.32601", "Beijing | 39.913818 | 116.363625"]);
