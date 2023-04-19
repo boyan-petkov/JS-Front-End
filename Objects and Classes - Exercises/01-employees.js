e719aa41e6502b68d68440a5ec4ca999612b964c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-19 09:54:30 +0300

    Initial commit

--- Objects and Classes - Exercises/01-employees.js
+++ Objects and Classes - Exercises/01-employees.js
@@ -1,1 +1,13 @@
     
+function employeesParser(input) {
+  for (const name of input) {
+    console.log(`Name: ${name} -- Personal Number: ${name.length}`);
+  }
+}
+
+employeesParser([
+  "Silas Butler",
+  "Adnaan Buckley",
+  "Juan Peterson",
+  "Brendan Villarreal",
+]);
