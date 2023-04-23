ee85447c2c0c0a213fc269043f0817713c48e5cb
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-23 11:42:20 +0300

    Initial commit

--- Objects and Classes - Exercises/06-word-tracker.js
+++ Objects and Classes - Exercises/06-word-tracker.js
@@ -1,1 +1,53 @@
 
+function wordsTracker(input) {
+  let searchedWords = input.shift().split(" ");
+  let words = [];
+  for (const word of searchedWords) {
+    let filterInput = input.filter((w) => w === word);
+    let count = filterInput.length;
+    words.push({ word, count });
+    let sortedWords = words.sort((word1, word2) => word2.count - word1.count);
+  }
+  for (const { word, count } of words) {
+    console.log(`${word} - ${count}`);
+  }
+}
+
+wordsTracker([
+  "this sentence",
+  "In",
+  "this",
+  "sentence",
+  "you",
+  "have",
+  "to",
+  "count",
+  "the",
+  "occurrences",
+  "of",
+  "the",
+  "words",
+  "this",
+  "and",
+  "sentence",
+  "because",
+  "this",
+  "is",
+  "your",
+  "task",
+]);
+
+wordsTracker([
+  "is the",
+  "first",
+  "sentence",
+  "Here",
+  "is",
+  "another",
+  "the",
+  "And",
+  "finally",
+  "the",
+  "the",
+  "sentence",
+]);
