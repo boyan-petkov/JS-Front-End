1cace8cb8311581c8b521b800e3cbfea5238193c
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-23 11:41:59 +0300

    Initial commit

--- Objects and Classes - Exercises/05-inventory.js
+++ Objects and Classes - Exercises/05-inventory.js
@@ -1,1 +1,23 @@
 
+function herosInventory(input) {
+  let heroes = [];
+  for (const currentHero of input) {
+    let [name, level, items] = currentHero.split(" / ");
+    level = Number(level);
+    heroes.push({ name, level, items });
+  }
+
+  let sortedHeroes = heroes.sort((heroA, heroB) => heroA.level - heroB.level);
+
+  for (const { name, level, items } of sortedHeroes) {
+    console.log(`Hero: ${name}`);
+    console.log(`level => ${level}`);
+    console.log(`items => ${items}`);
+  }
+}
+
+herosInventory([
+  "Isacc / 25 / Apple, GravityGun",
+  "Derek / 12 / BarrelVest, DestructionSword",
+  "Hes / 1 / Desolator, Sentinel, Antara",
+]);
