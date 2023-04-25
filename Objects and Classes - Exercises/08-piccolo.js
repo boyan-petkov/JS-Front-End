fd05931aa7b9450eeb66bfd3091ff89756c0bdef
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-04-25 12:08:24 +0300

    Initial commit

--- Objects and Classes - Exercises/08-piccolo.js
+++ Objects and Classes - Exercises/08-piccolo.js
@@ -1,1 +1,36 @@
 
+function piccolo(input) {
+  let carsInParking = [];
+  for (const carNumber of input) {
+    let [cmd, regNumber] = carNumber.split(", ");
+    if (cmd === "IN" && !carsInParking.includes(regNumber)) {
+      carsInParking.push(regNumber);
+    } else if (cmd === "OUT" && carsInParking.includes(regNumber)) {
+      let index = carsInParking.indexOf(regNumber);
+      carsInParking.splice(index, 1);
+    }
+  }
+  let sortedCars = carsInParking.sort((numA, numB) => numA.localeCompare(numB));
+  if (carsInParking.length > 0) {
+    sortedCars.forEach((element) => {
+      console.log(element);
+    });
+  } else {
+    console.log("Parking Lot is Empty");
+  }
+}
+
+piccolo([
+  "IN, CA2844AA",
+  "IN, CA1234TA",
+  "OUT, CA2844AA",
+  "IN, CA9999TT",
+  "IN, CA2866HI",
+  "OUT, CA1234TA",
+  "IN, CA2844AA",
+  "OUT, CA2866HI",
+  "IN, CA9876HH",
+  "IN, CA2822UU",
+]);
+
+piccolo(["IN, CA2844AA", "IN, CA1234TA", "OUT, CA2844AA", "OUT, CA1234TA"]);
