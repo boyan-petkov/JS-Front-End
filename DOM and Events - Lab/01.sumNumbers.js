7949d6cf6072631bc0dfcd5ed1ac6367603e7b28
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-05-02 16:10:50 +0300

    Initial commit

--- DOM and Events - Lab/01.sumNumbers.js
+++ DOM and Events - Lab/01.sumNumbers.js
@@ -1,1 +1,13 @@
 
+function calc() {
+    let firstNumberElement = document.getElementById('num1');
+    let secondNumberElement = document.getElementById('num2');
+
+    let firstNumber = Number(firstNumberElement.value);
+    let secondNumber = Number(secondNumberElement.value);
+
+    let sum = firstNumber + secondNumber;
+    
+    let resultElements = document.getElementById('sum');
+    resultElements.value = sum;
+}
