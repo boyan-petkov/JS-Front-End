d56c2f53454543f79b1bffd79c404e58c2a988ee
Boyan Petkov <96304825+boyan-petkov@users.noreply.github.com>
2023-05-02 16:11:38 +0300

    Initial commit

--- DOM and Events - Lab/03.collectListItems.js
+++ DOM and Events - Lab/03.collectListItems.js
@@ -1,1 +1,7 @@
 
+function extractText() {
+    let ulElement = document.getElementById('items');
+
+    let textareaElement = document.getElementById('result');
+    textareaElement.textContent = ulElement.textContent;
+}
